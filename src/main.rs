56e3182ff11e2a082b5babc2b423d5e53395c232
doramatadora <hello@doramilitaru.com>
2020-11-24 11:59:46 +0000

    tract wip

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use fastly::{Error, Request, Response};
+use tract_nnef::nnef;
+use tract_nnef::prelude::Framework;
+
+#[fastly::main]
+fn main(_req: Request) -> Result<Response, Error> {
+    let model = nnef()
+        //load the model
+        .model_for_path("mobilenet.nnef.tgz")?
+        // optimize graph
+        .into_optimized()?
+        // make the model runnable and fix its inputs and outputs
+        .into_runnable()?;
+
+    // // open image, resize it and make a Tensor out of it
+    // let image = image::open("image.jpg").unwrap().to_rgb8();
+    // let resized =
+    //     image::imageops::resize(&image, 224, 224, ::image::imageops::FilterType::Triangle);
+    // let image: Tensor = tract_ndarray::Array4::from_shape_fn((1, 3, 224, 224), |(_, c, y, x)| {
+    //     let mean = [0.485, 0.456, 0.406][c];
+    //     let std = [0.229, 0.224, 0.225][c];
+    //     (resized[(x as _, y as _)][c] as f32 / 255.0 - mean) / std
+    // })
+    // .into();
+
+    // // run the model on the input
+    // let result = model.run(tvec!(image))?;
+
+    // // find and display the max value with its index
+    // let best = result[0]
+    //     .to_array_view::<f32>()?
+    //     .iter()
+    //     .cloned()
+    //     .zip(2..)
+    //     .max_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+    // //let output: &str = include_str!("100MB.txt");
+    // // Make any desired changes to the client request.
+    // Ok(Response::from_body(format!("result: {:?}", best)))
+    Ok(Response::from_body("hi there"))
+}
